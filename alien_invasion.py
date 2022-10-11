536bf40bc3bf9ad22813b6b41a119c825838fc98
atabekdemurtaza <mychannel21war@gmail.com>
2022-10-11 15:43:23 +0500

    First commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,13 +32,19 @@ class AlienInvasion:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+			#Корабль не может одновременно двигать по обоим сторонам
 			elif event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_RIGHT:
 					#Двигаем кораблик на право
 					self.ship.moving_right = True
+				elif event.key == pygame.K_LEFT:
+					self.ship.moving_left = True
+
 			elif event.type == pygame.KEYUP:
 				if event.key == pygame.K_RIGHT:
-					self.ship.moving_right = False 
+					self.ship.moving_right = False
+				elif event.key == pygame.K_LEFT:
+					self.ship.moving_left = False
 				 
 	
 	def _update_screen(self):
