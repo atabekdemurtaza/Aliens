a1c803746524b7ff1e2afd68bfb9ab51c6928764
atabekdemurtaza <mychannel21war@gmail.com>
2022-10-11 15:51:12 +0500

    12345

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 class Settings:
 	#Настройки
 	def __init__(self):
-		#Установка
 		#Настройки экрана
 		self.screen_width = 800
 		self.screen_height = 600
 		self.bg_color = (221,250,198)
+
+		#Настройка корабля
+		self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -5,11 +5,19 @@ class Ship:
 	def __init__(self, ai_game):
 		self.screen = ai_game.screen
 		self.screen_rect = ai_game.screen.get_rect()
+		#Настройка
+		self.settings = ai_game.settings
+
 		#Загружаем иконку
 		self.image = pygame.image.load('images/ship.bmp')
 		self.rect  = self.image.get_rect()
+
 		#Локация
 		self.rect.midbottom = self.screen_rect.midbottom
+		
+		#Сохраяняем данные о корабля двигающийся по горизантали
+		self.x = float(self.rect.x)
+
 		#Движение кораблика
 		self.moving_right = False
 		self.moving_left  = False
@@ -17,9 +25,12 @@ class Ship:
 	def update(self):
 		#Обновляем движение
 		if self.moving_right:
-			self.rect.x += 1
+			self.x += self.settings.ship_speed
 		if self.moving_left:
-			self.rect.x -= 1
+			self.x -= self.settings.ship_speed
+
+		#Обновляем по горизонтали
+		self.rect.x = self.x
 
 	def blitme(self):
 		self.screen.blit(self.image, self.rect)
