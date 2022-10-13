1389dcb99ea924e4777617c1a62331f824bd98ab
atabekdemurtaza <mychannel21war@gmail.com>
2022-10-13 14:22:48 +0500

    13.10.2022

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
 		#Настройки экрана
 		self.screen_width = 800
 		self.screen_height = 600
-		self.bg_color = (221,250,198)
+		self.bg_color = (200,150,100)
 
 		#Настройка корабля
 		self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -24,9 +24,9 @@ class Ship:
 
 	def update(self):
 		#Обновляем движение
-		if self.moving_right:
+		if self.moving_right and self.rect.right < self.screen_rect.right:
 			self.x += self.settings.ship_speed
-		if self.moving_left:
+		if self.moving_left and self.rect.left > 0:
 			self.x -= self.settings.ship_speed
 
 		#Обновляем по горизонтали
