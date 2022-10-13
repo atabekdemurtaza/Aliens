b78be5e0463ecfad1fd2db386537653f0b164c79
atabekdemurtaza <mychannel21war@gmail.com>
2022-10-13 15:55:11 +0500

    15:55 13.10.2022

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
 		#Настройки экрана
 		self.screen_width = 800
 		self.screen_height = 600
-		self.bg_color = (200,150,100)
+		self.bg_color = (231,230,230)
 
 		#Настройка корабля
 		self.ship_speed = 1.5
