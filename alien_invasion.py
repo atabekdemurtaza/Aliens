5406f0640fcef21200a5627e88e01bd428bfa464
atabekdemurtaza <mychannel21war@gmail.com>
2022-10-18 14:47:36 +0500

    18.10.2022 14:47

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,16 +82,28 @@ class AlienInvasion:
 		#Создаем флот
 		#Cоздаем образ
 		alien = Alien(self)
-		alien_width = alien.rect.width
+		alien_width, alien_height = alien.rect.size
 		available_space_x = self.settings.screen_width - (2 * alien_width)
 		numbers_alien_x = available_space_x // (2 * alien_width)
 
-		for alien_number in range(numbers_alien_x):
-			#Создаем пришельца и измеряем локацию
-			alien = Alien(self)
-			alien.x = alien_width + (2 * alien_number * alien_width)
-			alien.rect.x = alien.x
-			self.aliens.add(alien)
+		# Сколько пришельцов могут поместиться на экране
+		ship_height = self.ship.rect.height
+		available_space_y = (self.settings.screen_height - 3*(alien_height) - ship_height)
+		number_rows = available_space_y // (2 * alien_height)
+
+		# Создадим полную версию флота пришельцев
+		for row_number in range(number_rows):
+			for alien_number in range(numbers_alien_x):
+				# Создаем пришельца и измеряем локацию
+				self._create_alien(alien_number, row_number)
+
+	def _create_alien(self, alien_number, row_number):
+		alien = Alien(self)
+		alien_width, alien_height = alien.rect.size
+		alien.x = alien_width + (2 * alien_number * alien_width)
+		alien.rect.x = alien.x
+		alien.rect.y = alien.rect.height + (2 * alien.rect.height * row_number)
+		self.aliens.add(alien)
 
 	def _update_screen(self):
 		#Рисовать
